5de16dc3d75f86ddf0fdd2a213ea320cc9d1e9e4
danielwang546 <42781202+danielwang546@users.noreply.github.com>
2022-09-17 17:43:28 -0400

    e

--- Grid.gd
+++ Grid.gd
@@ -19,8 +19,7 @@ var propTypes = ["varp", "varq", "implies", "lnot", "land", "lor"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	randomize()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- HackCMU PropLogic.service.worker.js
+++ HackCMU PropLogic.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1663433978|7598860117";
+const CACHE_VERSION = "1663449884|38356897";
 const CACHE_PREFIX = "HackCMU PropLogi-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "HackCMU PropLogic.offline.html";
